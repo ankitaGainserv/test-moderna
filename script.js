cdf3fbb22c7e567dcd91dec89c71b0e299823aa1
ankitaGainserv <104747857+ankitaGainserv@users.noreply.github.com>
2026-01-08 11:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-(function (u, z, a) {
-  if (u["a_id"]) {
-    return;
-  }
-  u["a_id"] = a;
-  u["dm"] = u.location.hostname;
-  var s = z.createElement("script");
-  s.async = true;
-  window.uzeraConfig = {
-    // replayMode: "always",
-    sdk_id: "I84N7SQKE7",
-    debug: true,
-  };
-  s.src = "https://assets.gainserv.in/wrapper.dev.min.js";
-  var t = z.getElementsByTagName("script")[0];
-  t.parentNode.insertBefore(s, t);
-})(window, document, "I84N7SQKE7");
+(function(u,z,a){if(u["a_id"]){return;}u["a_id"]=a;u["dm"]=u.location.hostname;u.uzera=u.uzera||[];u.uzera.q=u.uzera.q||[];['identify','trackEvent'].forEach(function(m){u.uzera[m]=function(){u.uzera.q.push([m,arguments]);};});var s=z.createElement("script");s.async=true;s.src="https://assets.gainserv.in/wrapper.dev.min.js";var t=z.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);})(window,document,"0PSMN7V282")
